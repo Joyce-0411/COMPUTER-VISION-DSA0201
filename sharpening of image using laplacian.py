b244af8b2b338600b3f87bde3cbc8acd7dc7a5d6
Joyce-0411 <120043101+Joyce-0411@users.noreply.github.com>
2023-05-17 10:41:17 +0530

    Add files via upload

---
+++ sharpening of image using laplacian.py
@@ -0,0 +1,26 @@
+a=imread("C:/Users/joyce/Downloads/3916115.webp");
+Lap=[0 1 0; 1 -4 1; 0 1 0];
+
+% Convolve the image read
+% in 'a' with Laplacian mask.
+a1=conv2(a,Lap,"C:/Users/joyce/Downloads/3916115.webp");
+
+% After convolution the intensity
+% Values go beyond the range.
+% Normalise the range of intensity.
+a2=uint8(a1);
+
+% Display the sharpened image.
+imtool(abs(a-a2),[])
+
+% Define strong laplacian filter
+lap=[-1 -1 -1; -1 8 -1; -1 -1 -1];
+
+% Apply filter on original image
+a3=conv2(a,lap,"C:/Users/joyce/Downloads/3916115.webp");
+
+% Normalise the resultant image.
+a4=uint8(a3);
+
+% Display the sharpened image.
+imtool(abs(a+a4),[])
